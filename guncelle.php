857debfa001ecdce1aa87f040d7d7901de12678c
ozgurince <oince026@gmail.com>
2014-04-26 22:19:25 +0300

    version 0.1.2 updated

--- guncelle.php
+++ guncelle.php
@@ -26,17 +26,17 @@
 	$yeniDeger = $_POST['yeniDeger'];		//En yakın zamanda yeniDeger kontrolü yapılıp buraya eklenecektir.
 											//Kullanıcı eğer sayı değiştirmek isterse, sayı kontrolü de yapılacaktır.
 
-	if ($guncellenecekAlan != "" && sayiControl($girilenNumara) && kelimeControl($yeniDeger)) {	//guncellenecekAlanı boş girmediyse ve sayi doğru girildiyse işlemler yapılır.
-	
-		$baglanti = mysqli_connect("localhost","root","","OgrenciBilgiSistemi");		//MySQL'e bağlanır.
-		$sonuclar = mysqli_query($baglanti,"SELECT * FROM ogrenciler ORDER BY `not` DESC");		
+	if($guncellenecekAlan == 'ad' || $guncellenecekAlan == 'soyad' || $guncellenecekAlan=='sinif'){
+		if (kelimeControl($yeniDeger)) {
+			$baglanti = mysqli_connect("localhost","root","","OgrenciBilgiSistemi");		//MySQL'e bağlanır.
+			$sonuclar = mysqli_query($baglanti,"SELECT * FROM ogrenciler ORDER BY `not` DESC");		
 
-		while ($row = mysqli_fetch_array($sonuclar)) {		// Sonuclar arasında tek tek karşılaştırma yaparız.
+			while ($row = mysqli_fetch_array($sonuclar)) {		// Sonuclar arasında tek tek karşılaştırma yaparız.
 					if ($row['numara'] == $girilenNumara) {
 						$flag1 = true;
 						mysqli_query($baglanti,"UPDATE ogrenciler SET `$guncellenecekAlan`= '$yeniDeger' WHERE `numara`= $girilenNumara");
 						echo "<h3>Öğrencini bilgileri güncellendi.</h3> <br>";
-						echo "<h3> Yeni Bilgiler </h3>";
+						echo "<h3> Eski Bilgiler </h3>";
 						echo "<h4>Adı: " . $row['ad'] . "<br>";
 						echo "Soyadı: " . $row['soyad'] . "<br>";
 						echo "Numarası: " . $row['numara'] . "<br>";
@@ -44,15 +44,59 @@
 						echo "Notu: " . $row['not'] . "<br></h4>";
 					}
 				}
+		
 				if (!$flag1) {
 					echo "<h1> Sistemde numarası $silinecekNumara olan öğrenci kaydı bulunmamaktadır. "
 			. "<br>Lutfen tekrar deneyiniz. </h1>";
 				}
+		}
 	}
 
-	else
-		echo "Lütfen geçerli değerler giriniz.";
+		if ($guncellenecekAlan == 'numara' || $guncellenecekAlan == 'not') {	
+			if (sayiControl($yeniDeger)) {
+				if ($guncellenecekAlan == 'not' && $yeniDeger < 100 && $yeniDeger > 0) {		//Not 0-100 aralığında olmalıdır.
+					$baglanti = mysqli_connect("localhost","root","","OgrenciBilgiSistemi");		//MySQL'e bağlanır.
+					$sonuclar = mysqli_query($baglanti,"SELECT * FROM ogrenciler ORDER BY `not` DESC");		
 
+					while ($row = mysqli_fetch_array($sonuclar)) {		// Sonuclar arasında tek tek karşılaştırma yaparız.
+						if ($row['numara'] == $girilenNumara) {
+							$flag2 = true;
+							mysqli_query($baglanti,"UPDATE ogrenciler SET `$guncellenecekAlan`= '$yeniDeger' WHERE `numara`= $girilenNumara");
+							echo "<h3>Öğrencini bilgileri güncellendi.</h3> <br>";
+							echo "<h3> Eski Bilgiler </h3>";
+							echo "<h4>Adı: " . $row['ad'] . "<br>";
+							echo "Soyadı: " . $row['soyad'] . "<br>";
+							echo "Numarası: " . $row['numara'] . "<br>";
+							echo "Sinifi: " . $row['sinif'] . "<br>";
+							echo "Notu: " . $row['not'] . "<br></h4>";
+						}
+					}
+				}
+				else{
+					$baglanti = mysqli_connect("localhost","root","","OgrenciBilgiSistemi");		//MySQL'e bağlanır.
+					$sonuclar = mysqli_query($baglanti,"SELECT * FROM ogrenciler ORDER BY `not` DESC");		
+
+					while ($row = mysqli_fetch_array($sonuclar)) {		// Sonuclar arasında tek tek karşılaştırma yaparız.
+						if ($row['numara'] == $girilenNumara) {
+							$flag2 = true;
+							mysqli_query($baglanti,"UPDATE ogrenciler SET `$guncellenecekAlan`= '$yeniDeger' WHERE `numara`= $girilenNumara");
+							echo "<h3>Öğrencini bilgileri güncellendi.</h3> <br>";
+							echo "<h3> Eski Bilgiler </h3>";
+							echo "<h4>Adı: " . $row['ad'] . "<br>";
+							echo "Soyadı: " . $row['soyad'] . "<br>";
+							echo "Numarası: " . $row['numara'] . "<br>";
+							echo "Sinifi: " . $row['sinif'] . "<br>";
+							echo "Notu: " . $row['not'] . "<br></h4>";
+						}
+					}
+				}
+				
+				if (!$flag2) {
+					echo "<h1> Sistemde numarası $silinecekNumara olan öğrenci kaydı bulunmamaktadır. "
+			. "<br>Lutfen tekrar deneyiniz. </h1>";
+				}		
+			}
+		}
 	function sayiControl($gelenSayi){
 		$flag = true;	// flag == kullanabilirlik kontrolu
 		$gelenSayi = trim($gelenSayi);
@@ -76,6 +120,7 @@
    	  	
    	  	return $flag;
 	}
+
 ?>
 </center>
 </body>
